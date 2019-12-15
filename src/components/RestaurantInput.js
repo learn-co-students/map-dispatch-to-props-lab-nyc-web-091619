c1eaa25b38f96ee33221992904b438325fc17f7e
Liam <lhealy0711@gmail.com>
2019-12-15 12:49:12 -0500

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -24,9 +24,11 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
+    console.log(this.props)
     return(
       <form onSubmit={(event) => this.handleOnSubmit(event)}>
         <p>
@@ -49,6 +51,5 @@ export class RestaurantInput extends Component {
   }
 };
 
-
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(state => ({ restaurants: state.restaurants }), {addRestaurant})(RestaurantInput)
